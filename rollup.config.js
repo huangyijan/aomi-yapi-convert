17abcfc741f47fcab19dad7d5563bc0fa1ea8666
yijian <1172256619.com>
2022-04-13 23:31:30 +0800

    :package: 配置插件运行环境，成功打包第一个版本

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,19 @@
 import typescript from 'rollup-plugin-typescript2';
+
+const override = { compilerOptions: { module: 'ESNEXT'}}
 export default {
   input: 'src/enter/index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs'
-  },
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      banner: '#!/usr/bin/env node'
+    }
+  ],
   plugins: [
-    typescript(/*{ plugin options }*/),
+    typescript({
+      tsconfigOverride: override
+    }),
 
   ]
 };
