b3aad4edc3f8e0edc822f2075d5a24653670ff4b
yijian <1172256619@qq.com>
2022-05-04 20:47:52 +0800

    :bug: change npm import index to empty chunk

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,36 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from 'rollup-plugin-typescript2'
 
 const override = { compilerOptions: { module: 'ESNEXT'}}
-export default {
-  input: 'src/enter/index.ts',
-  output: [
+export default [
     {
-      file: 'dist/index.js',
-      format: 'cjs',
-      banner: '#!/usr/bin/env node'
-    }
-  ],
-  plugins: [
-    typescript({
-      tsconfigOverride: override
-    }),
+        input: 'src/enter/index.ts',
+        output: [
+            {
+                file: 'dist/update.js',
+                format: 'cjs',
+                banner: '#!/usr/bin/env node'
+            }
+        ],
+        plugins: [
+            typescript({
+                tsconfigOverride: override
+            }),
 
-  ]
-};
+        ]
+    },
+    {
+        input: 'typings/index.ts',
+        output: [
+            {
+                file: 'dist/index.js',
+                format: 'cjs'
+            }
+        ],
+        plugins: [
+            typescript({
+                tsconfigOverride: override
+            }),
+
+        ]
+    }
+]
