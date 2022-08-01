3a69795caa1602316a7ae16a3ffa52f36ded567c
yijian <1172256619@qq.com>
2022-08-01 18:09:40 +0800

    :ambulance: fix multify level parse

--- src/prompt/response/ts.ts
+++ src/prompt/response/ts.ts
@@ -1,5 +1,5 @@
 import { getApiName, getType } from '../../utils/str-operate'
-import { removeProperties, getLegalJson } from '../../utils'
+import { removeProperties, getLegalJson, hasProperty } from '../../utils'
 import { getSecondNoteAndName } from '../second'
 import { dealResponseData, getReturnName } from '../note'
 import { getSuitableTsInterface, getSuitableTsType, getSuitableTsTypeNote, getSuitableType, getSuitDescription } from '../../utils/decision'
@@ -12,57 +12,57 @@ interface ReturnNoteStringItem {
 
 /** 配置返回注释 */
 export const getReturnNoteStringItem = (item: JsDocApiItem): ReturnNoteStringItem => {
-
     const body = getLegalJson(item.res_body) // 获取合法的json数据
-
     if (typeof body !== 'object') return { returnNameWithType: 'string', resType: '' }
-
     const requestName = getApiName(item.path, item.method)
-
-    const data = removeProperties(body) // 删除后台传回来的多余嵌套的属性数据
-
-    const { res, isArray } = dealResponseData(data) // 处理一下返回的数据
-
-
-    const returnName = getReturnName(requestName, res)
-
-    const resType = dealJsonToTsTypeReturn(res, returnName)
-
-
-    const returnNameWithType = isArray ? `${returnName}[]` : returnName
-
-    return { returnNameWithType, resType }
+    const returnName = requestName + 'Response'
+    const resType = dealJsonToTsTypeReturn(body, returnName)
+    return { returnNameWithType: returnName, resType }
 }
 
-/** chu */
 
 /** 处理返回的数据类型处理 */
-export const dealJsonToTsTypeReturn = (data: object, returnName: string) => {
+export const dealJsonToTsTypeReturn = (data: any, returnName: string) => {
+    if (!data || !data.properties || !Object.keys(data).length) return '' // 空的对象不做处理，提高性能
+    const bodyStr = getTsTypeStr(data.properties)
+    const resType = getSuitableTsInterface(returnName, bodyStr)
+    return resType
+}
 
+const getTsTypeStr = (data: object) => {
     let bodyStr = ''
-    let appendNoteJsdocType = '' // 额外的JsDocType
-
-
-    if (!Object.keys(data).length) return '' // 空的对象不做处理，提高性能
-
-    if (returnName === 'string' || returnName === 'array') return ''
-
-
     Object.entries(data).forEach(([key, value]) => {
         const description = getSuitDescription(value)
         let type = getSuitableType(value)
-        const addTypeName = getType(type, key, returnName)
-
-        const {note, name} = getSecondNoteAndName(value, addTypeName, type, appendNoteJsdocType)
-        
-        if(note) appendNoteJsdocType = note
-        if (name !== type) type = name
-        
+        if (type === 'object' && hasProperty(value, 'properties')) {
+            type = `{\n${getTsTypeStr(value.properties)}}\n`
+        }
+        if (type === 'array' && hasProperty(value, 'items')) {
+            type = getArrayDataStr(value)
+        }
         bodyStr += getSuitableTsTypeNote(description)
         bodyStr += getSuitableTsType(key, type)
     })
+    return bodyStr
+}
 
-    const resType = getSuitableTsInterface(returnName, bodyStr, appendNoteJsdocType)
+interface JsonSchema {
+    type: 'string' | 'integer' | 'number' | 'object' | 'array' | 'null' | 'boolean'
+    properties?: { [key: string]: object }
+    items?: JsonSchema
+    description?: string
+}
 
-    return resType
+
+const getArrayDataStr = (value: JsonSchema) => {
+    const child = value.items
+    let childType = getSuitableType(child)
+    if (childType === 'object' && child?.properties) {
+        childType = `{\n${getTsTypeStr(child.properties)}}`
+    }
+    if (childType === 'array' && child?.items) {
+        childType = getArrayDataStr(child)
+    }
+    childType = `Array<${childType}>`
+    return childType
 }
